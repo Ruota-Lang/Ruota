8dafe9865bb7843ef174fa92b754088b3faa2d69
Agilulfulus <parkbenjamin2@gmail.com>
2018-04-10 19:07:47 -0400

    Organization

--- Interpreter.h
+++ Interpreter.h
@@ -1,15 +1,15 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+#include "Tokenizer.h"
 #include<string>
 #include<map>
 #include<vector>
 #include<fstream>
 #include<iostream>
 #include<memory>
-#include "Tokenizer.h"
 #include<time.h>
-#include <windows.h>
+#include<windows.h>
 #include<chrono>
 #include<cmath>
 
@@ -29,53 +29,53 @@ enum NodeType {
 
 	MEM,
 	VAR,
-	LIST,
-	SOFT_LIST,
-	SCOPE,
-	SET,
-	SET_STAT,
-	OBJ_SET,
-	REF_SET,
-	ADD,
-	ADD_ARR,
-	STR_CAT,
-	SUB,
-	MUL,
-	DIV,
-	MOD,
-	POW,
+	LIST,		// [ ]
+	SOFT_LIST,	// ( )
+	SCOPE,		// { }
+	SET,		// =
+	SET_STAT,	// static
+	OBJ_SET,	// ::
+	REF_SET,	// :=
+	ADD,		// +
+	ADD_ARR,	// ++
+	STR_CAT,	// ..
+	SUB,		// -
+	MUL,		// *
+	DIV,		// /
+	MOD,		// %
+	POW,		// **
 
-	EQUAL,
-	NEQUAL,
-	LESS,
-	MORE,
-	ELESS,
-	EMORE,
-	AND,
-	OR,
+	EQUAL,		// ==
+	NEQUAL,		// !=
+	LESS,		// <
+	MORE,		// >
+	ELESS,		// <=
+	EMORE,		// >=
+	AND,		// &&
+	OR,			// ||
 
-	INDEX,
-	INDEX_OBJ,
-	SIZE_O,
-	EXEC,
-	EXEC_ITER,
-	FROM,
-	DES,
-	LDES,
+	INDEX,		// []
+	INDEX_OBJ,	// .
+	SIZE_O,		// #
+	EXEC,		// ()
+	EXEC_ITER,	// >>
+	FROM,		// from
+	DES,		// ->
+	LDES,		// =>
 
-	DOL,
-	THEN,
-	ITER,
-	RANGE,
+	DOL,		// do
+	THEN,		// then
+	ITER,		// in
+	RANGE,		// :
 
-	TYPE,
-	OUT_CALL,
-	NEW,
-	LOCAL,
-	STRUCT,
+	TYPE,		// &
+	OUT_CALL,	// _OUTER_CALL_
+	NEW,		// new
+	LOCAL,		// local
+	STRUCT,		// struct
 
-	BREAK,
-	RETURN
+	BREAK,		// break
+	RETURN		// return
 };
 
 enum MemType {
@@ -97,12 +97,13 @@ enum MemType {
 
 #define VEC_Memory	std::vector<SP_Memory>
 #define VEC_Node	std::vector<SP_Node>
+#define VEC_String	std::vector<std::string>
 
 
 struct Memory : std::enable_shared_from_this<Memory> {
 private:
 	static long	reference_count;
-	long double		data = 0;
+	long double	data = 0;
 	MemType		mt;
 	VEC_Memory	arr_data;
 	SP_Scope	obj_data = nullptr;
@@ -117,72 +118,73 @@ public:
 	Memory(long double);
 	Memory(SP_Lambda);
 	Memory(VEC_Memory);
-	SP_Memory add(const SP_Memory);
-	SP_Memory sub(const SP_Memory);
-	SP_Memory mul(const SP_Memory);
-	SP_Memory div(const SP_Memory);
-	SP_Memory mod(const SP_Memory);
-	SP_Memory pow(const SP_Memory);
-	bool equals(const SP_Memory);
-	bool isStatic();
-	bool isStruct();
-	void set(const SP_Memory);
-	long double getValue();
-	SP_Memory index(size_t pos);
-	SP_Memory index(std::string);
-	void setStatic(bool);
-	void setStruct(bool);
-	std::string toString();
-	SP_Memory clone(SP_Scope);
-	SP_Lambda getLambda();
-	SP_Scope getScope();
-	void setScope(SP_Scope);
-	void makeScope(SP_Scope);
-	void setType(MemType);
-	void refer(SP_Memory);
-	VEC_Memory getArray();
-	MemType getType();
+
+	SP_Memory 	add(const SP_Memory);
+	SP_Memory 	sub(const SP_Memory);
+	SP_Memory 	mul(const SP_Memory);
+	SP_Memory 	div(const SP_Memory);
+	SP_Memory 	mod(const SP_Memory);
+	SP_Memory 	pow(const SP_Memory);
+	bool 		equals(const SP_Memory);
+	bool 		isStatic();
+	bool 		isStruct();
+	void		set(const SP_Memory);
+	long double	getValue();
+	SP_Memory	index(size_t pos);
+	SP_Memory	index(std::string);
+	void		setStatic(bool);
+	void		setStruct(bool);
+	std::string	toString();
+	SP_Memory	clone(SP_Scope);
+	SP_Lambda	getLambda();
+	SP_Scope	getScope();
+	void		setScope(SP_Scope);
+	void		makeScope(SP_Scope);
+	void		setType(MemType);
+	void		refer(SP_Memory);
+	VEC_Memory	getArray();
+	MemType		getType();
 };
 
 struct Node : std::enable_shared_from_this<Node> {
-	VEC_Node params;
-	NodeType nt;
-	SP_Scope scope_ref = nullptr;
-	SP_Memory mem_data = nullptr;
-	std::string key;
-	SP_Memory execute(SP_Scope);
-	SP_Node clone(SP_Scope);
+	VEC_Node 	params;
+	NodeType 	nt;
+	SP_Scope 	scope_ref = nullptr;
+	SP_Memory 	mem_data = nullptr;
+	std::string	key;
 
 	Node(SP_Scope);
 	Node(long double);
 	Node(std::string);
 	Node(NodeType, VEC_Node);
 
-	std::string toString();
+	std::string	toString();
+	SP_Memory	execute(SP_Scope);
+	SP_Node		clone(SP_Scope);
 };
 
 struct Lambda : std::enable_shared_from_this<Lambda> {
-	SP_Node base = nullptr;
-	SP_Scope parent = nullptr;
-	std::vector<std::string> param_keys;
+	SP_Node		base = nullptr;
+	SP_Scope	parent = nullptr;
+	VEC_String	param_keys;
 
-	Lambda(SP_Scope, SP_Node, std::vector<std::string>);
-	SP_Memory execute(VEC_Memory);
-	SP_Lambda clone(SP_Scope);
+	Lambda(SP_Scope, SP_Node, VEC_String);
+	SP_Memory	execute(VEC_Memory);
+	SP_Lambda	clone(SP_Scope);
 };
 
 struct Scope : std::enable_shared_from_this<Scope> {
-	SP_Scope parent = nullptr;
-	SP_Node main = nullptr;
+	SP_Scope	parent = nullptr;
+	SP_Node		main = nullptr;
 	std::map<std::string, SP_Memory> variables;
+
 	Scope(SP_Scope);
 	Scope(SP_Scope, SP_Node);
-	SP_Memory execute();
-	SP_Memory getVariable(std::string);
 
-	SP_Scope clone(SP_Scope);
-
-	std::string toString();
+	SP_Memory	execute();
+	SP_Memory	getVariable(std::string);
+	SP_Scope	clone(SP_Scope);
+	std::string	toString();
 };
 
 class Interpreter {	
